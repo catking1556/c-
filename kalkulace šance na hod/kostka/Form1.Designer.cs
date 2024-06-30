b5637ecd66bc529e160b31442aeea78f10673adb
cinádr Jindřich <jindrisekcinadr@seznam.cz>
2024-06-30 20:44:34 +0200

    Add files via upload

---
+++ kalkulace šance na hod/kostka/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace kostka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(161, 290);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "pocet_hodnot";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(980, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(235, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hod";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button2.Location = new System.Drawing.Point(235, 230);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Vynulovat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = global::kostka.Properties.Resources._1;
+            this.pictureBox1.Image = global::kostka.Properties.Resources._6;
+            this.pictureBox1.Location = new System.Drawing.Point(520, 136);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(331, 124);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(208, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Počet hodů:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(208, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Interval (ms)";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(1010, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 225);
+            this.listBox1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(364, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(364, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1279, 591);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ kalkulace šance na hod/kostka/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace kostka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            nahoda = new Random();
+        }
+        Random nahoda;
+        int pocet_1 = 0;
+        int pocet_2 = 0;
+        int pocet_3 = 0;
+        int pocet_4 = 0;
+        int pocet_5 = 0;
+        int pocet_6 = 0;
+
+        private void graf()
+        { chart1.Series["pocet_hodnot"].Points.Clear();
+                chart1.Series["pocet_hodnot"].Points.AddXY("poc_1", pocet_1);
+            chart1.Series["pocet_hodnot"].Points.AddXY("poc_2", pocet_2);
+            chart1.Series["pocet_hodnot"].Points.AddXY("poc_3", pocet_3);
+            chart1.Series["pocet_hodnot"].Points.AddXY("poc_4", pocet_4);
+            chart1.Series["pocet_hodnot"].Points.AddXY("poc_5", pocet_5);
+            chart1.Series["pocet_hodnot"].Points.AddXY("poc_6", pocet_6);
+            chart1.Update();
+        }
+               private void button1_Click(object sender, EventArgs e)
+        {
+            int cas = int.Parse(textBox2.Text);
+            int hody = int.Parse(textBox1.Text);
+            for (int i = 1; i <= hody; i++) {
+                listBox1.Items.Clear();
+                int cislo = nahoda.Next(1, 7);
+                switch (cislo) {
+                    case 1:
+                        pocet_1 += 1;
+                        pictureBox1.Image = Image.FromFile(@"1.jpg");
+                        pictureBox1.Refresh();
+                        Thread.Sleep(cas);
+                        break;
+                    case 2:
+                        pocet_2 += 1;
+                        pictureBox1.Image = Image.FromFile(@"2.jpg");
+                        pictureBox1.Refresh();
+                        Thread.Sleep(cas);
+                        break;
+                    case 3:
+                        pocet_3 += 1;
+                        pictureBox1.Image = Image.FromFile(@"3.jpg");
+                        pictureBox1.Refresh();
+                        Thread.Sleep(cas);
+                        break;
+                    case 4:
+                        pocet_4 += 1;
+                        pictureBox1.Image = Image.FromFile(@"4.jpg");
+                        pictureBox1.Refresh();
+                        Thread.Sleep(cas);
+                        break;
+                    case 5:
+                        pocet_5 += 1;
+                        pictureBox1.Image = Image.FromFile(@"5.jpg");
+                        pictureBox1.Refresh();
+                        Thread.Sleep(cas);
+                        break;
+                    case 6:
+                        pocet_6 += 1;
+                        pictureBox1.Image = Image.FromFile(@"6.jpg");
+                        pictureBox1.Refresh();
+                        Thread.Sleep(cas);
+                        break;
+                }
+                listBox1.Items.Add("počet jedniček:" + pocet_1.ToString());
+                listBox1.Items.Add("počet dvojek:" + pocet_2.ToString());
+                listBox1.Items.Add("počet trojek:" + pocet_3.ToString());
+                listBox1.Items.Add("počet čtyřek:" + pocet_4.ToString());
+                listBox1.Items.Add("počet pětek:" + pocet_5.ToString());
+                listBox1.Items.Add("počet šestek:" + pocet_6.ToString());
+                listBox1.Items.Add("celkem hodů:" + i.ToString());
+                listBox1.Refresh();
+                graf();
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pocet_1 = 0;
+            pocet_2 = 0;
+            pocet_3 = 0;
+            pocet_4 = 0;
+            pocet_5 = 0;
+            pocet_6 = 0;
+            listBox1.Items.Clear();
+            listBox1.Items.Add("počet jedniček:" + pocet_1.ToString());
+            listBox1.Items.Add("počet dvojek:" + pocet_2.ToString());
+            listBox1.Items.Add("počet trojek:" + pocet_3.ToString());
+            listBox1.Items.Add("počet čtyřek:" + pocet_4.ToString());
+            listBox1.Items.Add("počet pětek:" + pocet_5.ToString());
+            listBox1.Items.Add("počet šestek:" + pocet_6.ToString());
+            graf();
+        }
+    }
+}
